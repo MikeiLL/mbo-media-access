34bc1a24fe51a782cbe8ce0fdd8d4804fc45e3a0
Mike iLL Kilmer <mike@mzoo.org>
2021-01-20 17:38:17 -0600

    Remove video for now as seems broken.

--- includes/modules/index.js
+++ includes/modules/index.js
@@ -1,9 +1,7 @@
 import HelloWorld from './HelloWorld/HelloWorld';
 import MindbodyAudio from './MindbodyAudio/MindbodyAudio';
-import MindbodyVideo from './MindbodyVideo/MindbodyVideo';
 
 export default [
     HelloWorld,
-    MindbodyAudio,
-    MindbodyVideo
+    MindbodyAudio
 ];
